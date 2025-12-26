1a3b5b74a3753fddfc802e0e3f2174c29ba58da5
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2025-12-26 11:54:48 +0100

    feat(day06): add part 2

--- day06.go
+++ day06.go
@@ -1,89 +1,144 @@
 package adventofcode2025
 
-type Day06Puzzle struct {
-	nums []uint // flat 2D: nums[row*cols+col], 1 alloc, contiguous
-	ops  []byte
+func Day06(lines []string, part1 bool) uint {
+	if part1 {
+		return day06Part1(lines)
+	}
+	return day06Part2(lines)
 }
 
-func NewDay06(lines []string) (Day06Puzzle, error) {
+func day06Part1(lines []string) uint {
 	h := len(lines)
 
-	countTokens := func(line string) int {
-		count := 0
-		inToken := false
-		for i := range len(line) {
-			if line[i] == ' ' {
-				inToken = false
-			} else if !inToken {
-				inToken = true
-				count++
-			}
+	// track position in each row
+	pos := make([]int, h)
+
+	nextToken := func(row int) (start, end int, ok bool) {
+		line := lines[row]
+		p := pos[row]
+		for p < len(line) && line[p] == ' ' {
+			p++
 		}
-		return count
+		if p >= len(line) {
+			return 0, 0, false
+		}
+		start = p
+		for p < len(line) && line[p] != ' ' {
+			p++
+		}
+		pos[row] = p
+		return start, p, true
 	}
 
-	parseOps := func(line string, ops []byte) {
-		col := 0
-		for i := range len(line) {
-			c := line[i]
-			if c == '+' || c == '*' {
-				ops[col] = c
-				col++
-			}
-		}
-	}
-
-	parseNums := func(line string, nums []uint) {
-		col := 0
-		var n uint
-		inNum := false
-		for i := range len(line) {
-			c := line[i]
-			if c >= '0' && c <= '9' {
-				n = n*10 + uint(c-'0')
-				inNum = true
-			} else if inNum {
-				nums[col] = n
-				col++
-				n = 0
-				inNum = false
-			}
-		}
-		if inNum {
-			nums[col] = n
-		}
-	}
-
-	cols := countTokens(lines[h-1])
-	rows := h - 1
-
-	nums := make([]uint, rows*cols)
-	ops := make([]byte, cols)
-
-	parseOps(lines[h-1], ops)
-
-	for i := range rows {
-		parseNums(lines[i], nums[i*cols:(i+1)*cols])
-	}
-
-	return Day06Puzzle{nums: nums, ops: ops}, nil
-}
-
-func Day06(p Day06Puzzle, part1 bool) uint {
-	cols := len(p.ops)
-	rows := len(p.nums) / cols
 	var total uint
-	for col := range cols {
+	for {
+		// get operator from last row
+		opStart, _, ok := nextToken(h - 1)
+		if !ok {
+			break
+		}
+		op := lines[h-1][opStart]
+
 		var result uint
-		if p.ops[col] == '*' {
+		if op == '*' {
 			result = 1
 		}
-		for row := range rows {
-			n := p.nums[row*cols+col]
-			if p.ops[col] == '+' {
+
+		for row := range h - 1 {
+			start, end, ok := nextToken(row)
+			if !ok {
+				continue
+			}
+			var n uint
+			for i := start; i < end; i++ {
+				c := lines[row][i]
+				if c >= '0' && c <= '9' {
+					n = n*10 + uint(c-'0')
+				}
+			}
+			if op == '+' {
 				result += n
 			} else {
 				result *= n
+			}
+		}
+		total += result
+	}
+	return total
+}
+
+func day06Part2(lines []string) uint {
+	h := len(lines)
+	w := 0
+	for _, line := range lines {
+		if len(line) > w {
+			w = len(line)
+		}
+	}
+
+	getChar := func(row, x int) byte {
+		if x < len(lines[row]) {
+			return lines[row][x]
+		}
+		return ' '
+	}
+
+	isSpaceCol := func(x int) bool {
+		for row := range h {
+			if getChar(row, x) != ' ' {
+				return false
+			}
+		}
+		return true
+	}
+
+	var total uint
+	x := 0
+	for x < w {
+		for x < w && isSpaceCol(x) {
+			x++
+		}
+		if x >= w {
+			break
+		}
+
+		x0 := x
+		for x < w && !isSpaceCol(x) {
+			x++
+		}
+		x1 := x
+
+		// find operator
+		var op byte
+		for i := x0; i < x1; i++ {
+			c := getChar(h-1, i)
+			if c == '+' || c == '*' {
+				op = c
+				break
+			}
+		}
+
+		// process character columns right-to-left
+		var result uint
+		if op == '*' {
+			result = 1
+		}
+		for charCol := x1 - 1; charCol >= x0; charCol-- {
+			var n uint
+			hasDigit := false
+			for row := range h - 1 {
+				c := getChar(row, charCol)
+				if c >= '0' && c <= '9' {
+					n = n*10 + uint(c-'0')
+					hasDigit = true
+				}
+			}
+			if hasDigit {
+				if op == '+' {
+					result += n
+				} else {
+					result *= n
+				}
 			}
 		}
 		total += result
--- day06_test.go
+++ day06_test.go
@@ -3,13 +3,25 @@ package adventofcode2025
 import "testing"
 
 func TestDay06Part1Example(t *testing.T) {
-	testWithParser(t, 6, exampleFilename, true, NewDay06, Day06, 4277556)
+	testLines(t, 6, exampleFilename, true, Day06, 4277556)
 }
 
 func TestDay06Part1(t *testing.T) {
-	testWithParser(t, 6, filename, true, NewDay06, Day06, 3525371263915)
+	testLines(t, 6, filename, true, Day06, 3525371263915)
 }
 
 func BenchmarkDay06Part1(b *testing.B) {
-	benchWithParser(b, 6, true, NewDay06, Day06)
+	benchLines(b, 6, true, Day06)
+}
+
+func TestDay06Part2Example(t *testing.T) {
+	testLines(t, 6, exampleFilename, false, Day06, 3263827)
+}
+
+func TestDay06Part2(t *testing.T) {
+	testLines(t, 6, filename, false, Day06, 6846480843636)
+}
+
+func BenchmarkDay06Part2(b *testing.B) {
+	benchLines(b, 6, false, Day06)
 }
